7bdd2f7135b0fdc96b1906fc86226174768653c1
Josh-Green <joshgreen95@protonmail.com>
2023-05-02 22:53:53 +0100

    I dont know how it works but it works

--- src/Three/shaders/background/vertex.glsl
+++ src/Three/shaders/background/vertex.glsl
@@ -20,12 +20,11 @@ void main(){
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    float sumIntersectSquare = ((uRaycastIntersect.x * uRaycastIntersect.x) + (uRaycastIntersect.y * uRaycastIntersect.y));
-    float sumXYSquare =  ((modelPosition.x * modelPosition.x ) + (modelPosition.z * modelPosition.z)) ;
 
     // 5 is model size????
-    vec2 distanceFromIntersect = vec2(distance(modelPosition.x - ((uRaycastIntersect.x * 2.0  ) * 5.0), (uRaycastIntersect.x * 2.0 - 1.0 * 5.0)),
-                                 distance(modelPosition.z - ((uRaycastIntersect.y * 2.0) * 5.0), (uRaycastIntersect.y * 2.0 - 1.0)));
+    vec2 distanceFromIntersect = vec2(distance(modelPosition.x , ((uRaycastIntersect.x * 2.0  - 1.0) * 5.0)),
+                                 distance(modelPosition.z , ((uRaycastIntersect.y * 2.0 - 1.0) * 5.0)));
+
 
     float sumDistanceSquare = ((distanceFromIntersect.x * distanceFromIntersect.x) + (distanceFromIntersect.y * distanceFromIntersect.y));
 
