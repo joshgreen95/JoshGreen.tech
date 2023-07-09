76420f66137dffd5465e85242ba2c510ca99ca0d
Josh-Green <joshgreen95@protonmail.com>
2023-07-09 01:30:28 +0100

    Added Floatables
    Added Floatables Class.

--- src/Three/Props/Floatables/AnimateFloatables.js
+++ src/Three/Props/Floatables/AnimateFloatables.js
@@ -1,6 +1,9 @@
 import { GetDistance } from "../../Logic/GetDistance";
 
 function AnimateFloatable(floatable, bathWaterMaterial) {
+    if (!floatable.model) { return; }
+
+    const floatableModel = floatable.model;
     const amplitude = bathWaterMaterial.uniforms.uWaveAmplitude.value;
     const dampening = bathWaterMaterial.uniforms.uWaveDampening.value;
     const frequency = bathWaterMaterial.uniforms.uWaveFrequency.value;
@@ -8,15 +11,15 @@ function AnimateFloatable(floatable, bathWaterMaterial) {
     const decayTime = bathWaterMaterial.uniforms.uDecayTime.value;
     const raycastIntersectWorld = bathWaterMaterial.uniforms.uRaycastIntersectWorld.value;
 
+
     let clampedTime = Math.min(Math.max(elapsedTime, 0), decayTime);
     let timePercentage = (1 - (clampedTime / decayTime));
     let adjustedAmplitude = timePercentage * amplitude;
-    let distanceFromIntersect = GetDistance(floatable.position, raycastIntersectWorld);
+    let distanceFromIntersect = GetDistance(floatableModel.position, raycastIntersectWorld);
     let squareDistance = Math.pow(distanceFromIntersect, 2);
-
     let height = 1 - adjustedAmplitude * Math.cos(squareDistance + elapsedTime * frequency) * Math.exp(-dampening * squareDistance);
 
-    floatable.position.y = height;
+    floatableModel.position.y = height;
 }
 
 
--- src/Three/Props/Floatables/Floatable.js
+++ src/Three/Props/Floatables/Floatable.js
@@ -1,6 +1,31 @@
-class Floatable{
-    constructor(model, pagedata){
-        this.model = model;
-        this.pagedata = pagedata;
+import { LoadGLTFScene } from "../ImportModel";
+
+class Floatable {
+    constructor(modelRef, modelScale, pageRef, scene, waterMesh) {
+        this.modelRef = modelRef;
+        this.model = null;
+        this.page = pageRef;
+        this.scale = modelScale;
+        this.scene = scene;
+        this.waterMesh = waterMesh;
+    }
+
+    Instantiate() {
+        LoadGLTFScene(this.scene, this.modelRef, this.loadCallback, this);
+    }
+
+    PlaceRandomly() {
+        console.log(this.waterMesh);
+    }
+
+    loadCallback(self, model) {
+        self.model = model;
+        model.scale.setScalar(self.scale);
+        console.log(self.model);
+
+        //Random Placement;
+        self.PlaceRandomly();
     }
 }
+
+export { Floatable };
