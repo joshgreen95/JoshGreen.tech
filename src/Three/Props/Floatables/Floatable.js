f4c11b18f162d960935e48a6fe9f16a1e2574113
Josh-Green <joshgreen95@protonmail.com>
2023-07-10 02:22:59 +0100

    Update Floatable.js
    Model is assigned and instantiate called in constructor.

--- src/Three/Props/Floatables/Floatable.js
+++ src/Three/Props/Floatables/Floatable.js
@@ -3,22 +3,14 @@ import { LoadGLTFScene } from "../ImportModel";
 class Floatable {
     constructor(modelRef, modelScale, pageRef, scene, waterMesh) {
         this.modelRef = modelRef;
-        this.model = null;
         this.page = pageRef;
         this.scale = modelScale;
         this.scene = scene;
         this.waterMesh = waterMesh;
+        this.model = LoadGLTFScene(this.scene, this.modelRef, this.Instantiate, this);
     }
 
-    Instantiate() {
-        LoadGLTFScene(this.scene, this.modelRef, this.loadCallback, this);
-    }
-
-    PlaceRandomly() {
-        console.log(this.waterMesh);
-    }
-
-    loadCallback(self, model) {
+    Instantiate(self, model) {
         self.model = model;
         model.scale.setScalar(self.scale);
         console.log(self.model);
@@ -26,6 +18,11 @@ class Floatable {
         //Random Placement;
         self.PlaceRandomly();
     }
+
+    PlaceRandomly() {
+        console.log(this.waterMesh);
+    }
+
 }
 
 export { Floatable };
