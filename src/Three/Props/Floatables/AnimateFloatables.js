ff11adcb7a13368eba4508ce0d4fd2e8a2496a97
Josh-Green <joshgreen95@protonmail.com>
2023-07-12 02:17:20 +0100

    Added place Randomly Method to Floatables.

--- src/Three/Props/Floatables/AnimateFloatables.js
+++ src/Three/Props/Floatables/AnimateFloatables.js
@@ -1,9 +1,8 @@
 import { GetDistance } from "../../Logic/GetDistance";
 
 function AnimateFloatable(floatable, bathWaterMaterial) {
-    if (!floatable.model) { return; }
+    if (!floatable) { return; }
 
-    const floatableModel = floatable.model;
     const amplitude = bathWaterMaterial.uniforms.uWaveAmplitude.value;
     const dampening = bathWaterMaterial.uniforms.uWaveDampening.value;
     const frequency = bathWaterMaterial.uniforms.uWaveFrequency.value;
@@ -11,15 +10,14 @@ function AnimateFloatable(floatable, bathWaterMaterial) {
     const decayTime = bathWaterMaterial.uniforms.uDecayTime.value;
     const raycastIntersectWorld = bathWaterMaterial.uniforms.uRaycastIntersectWorld.value;
 
-
     let clampedTime = Math.min(Math.max(elapsedTime, 0), decayTime);
     let timePercentage = (1 - (clampedTime / decayTime));
     let adjustedAmplitude = timePercentage * amplitude;
-    let distanceFromIntersect = GetDistance(floatableModel.position, raycastIntersectWorld);
+    let distanceFromIntersect = GetDistance(floatable.position, raycastIntersectWorld);
     let squareDistance = Math.pow(distanceFromIntersect, 2);
     let height = 1 - adjustedAmplitude * Math.cos(squareDistance + elapsedTime * frequency) * Math.exp(-dampening * squareDistance);
 
-    floatableModel.position.y = height;
+    floatable.position.y = height;
 }
 
 
--- src/Three/Props/Floatables/Floatable.js
+++ src/Three/Props/Floatables/Floatable.js
@@ -14,18 +14,37 @@ class Floatable {
     Instantiate(self, model) {
         self.model = model;
         model.scale.setScalar(self.scale);
-        console.log(self.model);
 
         //Random Placement;
         self.PlaceRandomly();
     }
 
     PlaceRandomly() {
-        console.log(this.waterMesh);
+        console.log(this.model);
+        const waterPosX = this.waterMesh.position.x;
+        const waterPosZ = this.waterMesh.position.z;
+        const waterWidth = this.waterMesh.geometry.parameters.width;
+        const waterHeight = this.waterMesh.geometry.parameters.height;
+
+        const boundaryOffset = 3;
+        const offsetWidth = waterWidth - boundaryOffset;
+        const offsetHeight = waterHeight - boundaryOffset
+
+        let xPos = (((Math.random() * (offsetWidth)) - (offsetWidth / 2)) + waterPosX);
+        let zPos = (((Math.random() * offsetHeight) - (offsetHeight / 2)) + waterPosZ);
+
+        console.log(xPos, zPos);
+        this.model.position.x = xPos;
+        this.model.position.z = zPos;
+
+        const rotationalFactor = Math.PI * 0.75;
+        this.model.rotation.y = rotationalFactor / 2 - ((Math.random() * rotationalFactor));
     }
 
     Float() {
-        AnimateFloatable(this.model, this.waterMesh.material);
+        if (this.model) {
+            AnimateFloatable(this.model, this.waterMesh.material);
+        }
     }
 }
 
