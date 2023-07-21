90987b4d10d311bc8e66988a82bca0d1f5d8945f
Josh-Green <joshgreen95@protonmail.com>
2023-07-21 15:07:57 +0100

    Create SceneSetter.js

---
+++ src/Three/Props/SceneSetter.js
@@ -0,0 +1,11 @@
+import { CameraIndex } from "../Camera/CameraIndex.js";
+import { LoadGLTFScene } from "./ImportModel.js";
+
+class SceneSetter{
+    constructor(hitBox, scene, cameraIndex){
+        this.hitBox = hitBox
+        this.cameraIndex = cameraIndex;
+
+    }
+}
+
