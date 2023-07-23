7b00831e3c8cbaa404f6f48f7716aa9e31e0c681
Josh-Green <joshgreen95@protonmail.com>
2023-07-23 19:36:45 +0100

    Update SceneSetter.js

--- src/Three/Props/SceneSetter.js
+++ src/Three/Props/SceneSetter.js
@@ -1,11 +1,18 @@
+import * as THREE from 'three';
 import { CameraIndex } from "../Camera/CameraIndex.js";
 import { LoadGLTFScene } from "./ImportModel.js";
+import { AssignTagToScene } from '../Logic/AssignTagsToScene.js';
 
 class SceneSetter{
-    constructor(hitBox, scene, cameraIndex){
-        this.hitBox = hitBox
+    constructor(hitBoxSize, hitboxPosition, scene, cameraIndex){
+        this.hitboxGeometry = new THREE.BoxGeometry(hitBoxSize.x, hitBoxSize.y, hitBoxSize.z);
+        this.hitboxMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+        this.hitboxMesh = new THREE.Mesh(this.hitboxGeometry, this.hitboxMaterial);
         this.cameraIndex = cameraIndex;
 
+        this.hitboxMesh.position = hitboxPosition;
+        AssignTagToScene(this.hitboxGeometry, 'sceneSetter', true);
+        scene.add(this.hitboxMesh);
     }
 }
 
