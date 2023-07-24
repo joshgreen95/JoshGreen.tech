3213dec24266ca864218a9236973966d534e328d
Josh-Green <joshgreen95@protonmail.com>
2023-07-24 22:33:45 +0100

    Update SceneSetter.js

--- src/Three/Props/SceneSetter.js
+++ src/Three/Props/SceneSetter.js
@@ -6,7 +6,7 @@ import { AssignTagToScene } from '../Logic/AssignTagsToScene.js';
 class SceneSetter{
     constructor(hitBoxSize, hitboxPosition, scene, cameraIndex){
         this.hitboxGeometry = new THREE.BoxGeometry(hitBoxSize.x, hitBoxSize.y, hitBoxSize.z);
-        this.hitboxMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+        this.hitboxMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: false});
         this.hitboxMesh = new THREE.Mesh(this.hitboxGeometry, this.hitboxMaterial);
         this.cameraIndex = cameraIndex;
 
