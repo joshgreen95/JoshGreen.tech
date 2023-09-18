453b5a615f4873e7de3838c458a702ab6609b411
Josh-Green <joshgreen95@protonmail.com>
2023-09-19 00:16:42 +0100

    Added CV Label
    Addeed label to cv button, pruned sound

--- src/React/Pages/Header.jsx
+++ src/React/Pages/Header.jsx
@@ -6,6 +6,7 @@ import { links } from '../../Resources/Links.js';
 export default function Header() {
   return (
     <>
+        <h9>CV</h9>
         <i className="fa-regular fa-file fa-2x fixedButton" onClick={() => { window.open('/files/Joshua Green - Resume.pdf'); }}></i>
         <i className="fa-brands fa-github fa-2x fixedButton" onClick={() => { window.open(links['github'], '_blank').focus(); }}></i>
         <i className="fa-brands fa-twitter fa-2x fixedButton" onClick={() => { window.open(links['twitter'], '_blank').focus(); }}></i>
