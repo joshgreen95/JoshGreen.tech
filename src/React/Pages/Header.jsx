123d5b72517385a644b8741158c96b4ce8062362
Josh-Green <joshgreen95@protonmail.com>
2023-10-06 18:57:54 +0100

    Updated Resume

--- src/React/Pages/Header.jsx
+++ src/React/Pages/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <>
         <h9>CV</h9>
-        <i className="fa-regular fa-file fa-2x fixedButton" onClick={() => { window.open('/files/Joshua Green - Resume.pdf'); }}></i>
+      <i className="fa-regular fa-file fa-2x fixedButton" onClick={() => { window.open('/files/Josh_Green_Resume.pdf'); }}></i>
         <i className="fa-brands fa-github fa-2x fixedButton" onClick={() => { window.open(links['github'], '_blank').focus(); }}></i>
         <i className="fa-brands fa-linkedin fa-2x fixedButton" onClick={() => { window.open(links['linkedin'], '_blank').focus(); }}></i>
         <i className="fa-solid fa-info fa-2x fixedButton" onClick={() => { PageManager.ShowOverlay(infoScreen, PageManager.activeCamera, 'ui'); }}></i>
